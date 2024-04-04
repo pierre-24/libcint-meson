2833b46c6c94e724eb764eb34319da5fe5060f4f
pierre-24 <pierreb24@gmail.com>
2024-04-04 07:36:18 +0200

    mv

--- patch.sh
+++ patch.sh
@@ -11,13 +11,13 @@ echo "... Patched!"
 
 echo "Generate C files and corresponding header"
 cd scripts/
-cat ../../base_cint_funcs.h > cint_funcs.h
+cat ../../for-libcint/base_cint_funcs.h > cint_funcs.h
 clisp auto_intor.cl >> cint_funcs.h
 mv *.c ../src/autocode/
 mv cint_funcs.h ../include/
 cd ..
 
 echo "add Meson files"
-cp -R ../meson/* .
+cp -R ../for-meson/* .
 
 echo "done"
